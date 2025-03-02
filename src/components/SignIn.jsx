5c2aa2bc5337dd6f2e2c8bee4523eeeae08de9c6
Ahmed Mohamed Ezzat <158809486+Costtta@users.noreply.github.com>
2025-03-02 19:12:46 +0200

    simple changes

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,9 +1,11 @@
-import { Box, Button, Grid2, TextField, Typography, useTheme } from '@mui/material';
+import { Box, Button, Grid2, IconButton, Input, InputAdornment, Typography, useTheme } from '@mui/material';
 import React, { useState } from 'react'
 import LockIcon from '@mui/icons-material/Lock';
 import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchSignIn } from '../redux/signInSlice';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 
 
 const SignIn = () => {
@@ -15,26 +17,36 @@ const SignIn = () => {
         e.preventDefault();
         dispatch(fetchSignIn({ email, password }))
     }
+    const [visable, setVisable] = useState(false)
+    const handleVisability = () =>{
+        setVisable(!visable)
+    }
 
 
     const theme = useTheme()
     return (
         // <Box component={'section'} position={'relative'} className='sign-in-up' marginTop={14}>
-                    <Grid2 size={{ md: 6, xs: 12 }} bgcolor={theme.palette.grey[500]} paddingTop={4.5} marginBottom={4.5}>
-                        <Typography sx={{ textTransform: 'uppercase' }} textAlign={'center'} fontWeight={'bold'}>I am already a customer</Typography>
-                        <form action="" onSubmit={handleSignIn}>
-                            <Box display={'flex'} alignItems={'end'} paddingTop={6}>
-                                <AlternateEmailIcon sx={{ mr: 1, my: 0.5 }} />
-                                <TextField type='email' variant='standard' sx={{ width: '100%' }} label='E-mail Address' value={email} onChange={(e) => setEmail(e.target.value)} required />
-                            </Box>
-                            <br />
-                            <Box display='flex' alignItems='flex-end' marginBottom={19}>
-                                <LockIcon sx={{ mr: 1, my: 0.5 }} />
-                                <TextField sx={{ width: '100%' }} label='Password' variant='standard' type='password' value={password} onChange={(e) => setPassword(e.target.value)} required />
-                            </Box>
-                            <Button type='submit' variant='contained' color='warning' sx={{ width: '100%', borderRadius: '18px', color: 'white', textTransform: 'capitalize' }}>Sign in</Button>
-                        </form>
-                    </Grid2>
+        <Grid2 size={{ md: 6, xs: 12 }} bgcolor={theme.palette.grey[500]} paddingTop={4.5} marginBottom={4.5}>
+            <Typography sx={{ textTransform: 'uppercase' }} textAlign={'center'} fontWeight={'bold'}>I am already a customer</Typography>
+            <form action="" onSubmit={handleSignIn}>
+                <Box display={'flex'} alignItems={'end'} paddingTop={6}>
+                    <AlternateEmailIcon sx={{ mr: 1, my: 0.5 }} />
+                    <Input type='email' variant='standard' sx={{ width: '100%' }} placeholder='E-mail Address' value={email} onChange={(e) => setEmail(e.target.value)} required />
+                </Box>
+                <br />
+                <Box display='flex' alignItems='flex-end' marginBottom={40}>
+                    <LockIcon sx={{ mr: 1, my: 0.5 }} />
+                    <Input sx={{ width: '100%' }} placeholder='Password' variant='standard' type='password' value={password} onChange={(e) => setPassword(e.target.value)} required endAdornment={
+                        <InputAdornment>
+                            <IconButton onClick={handleVisability}>
+                                {!visable ? <VisibilityOffIcon /> : <Visibility /> }
+                            </IconButton>
+                        </InputAdornment>
+                    } />
+                </Box>
+                <Button type='submit' variant='contained' color='warning' sx={{ width: '100%', borderRadius: '18px', color: 'white', textTransform: 'capitalize' }}>Sign in</Button>
+            </form>
+        </Grid2>
         // </Box>
     )
 }
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Grid2, TextField, Typography, useTheme } from "@mui/material"
+import { Box, Button, Grid2, IconButton, Input, InputAdornment, Typography, useTheme } from "@mui/material"
 import LockIcon from '@mui/icons-material/Lock';
 import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
 import CircleIcon from '@mui/icons-material/Circle';
@@ -6,23 +6,27 @@ import { FcGoogle } from "react-icons/fc";
 import { useDispatch, useSelector } from "react-redux";
 import { useState } from "react";
 import { fetchSignUp } from "../redux/signUpSlice";
-
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 
 const SignUp = () => {
     const theme = useTheme()
     const [name, setFirstName] = useState('')
     const [lastname, setLastName] = useState('')
-    const [password, setPasswordUp] = useState('')
-    const [email, setEmailUp] = useState('')
+    const [password, setPassword] = useState('')
+    const [email, setEmail] = useState('')
     const { data2, isLoading2, error2 } = useSelector(state => state.signUp)
     const dispatch = useDispatch()
-    
-    
+
     const handleSignUp = (e) => {
         e.preventDefault()
-        dispatch(fetchSignUp({name, lastname, password, email}))
+        dispatch(fetchSignUp({ name, lastname, password, email }))
     }
-    
+        const [visable, setVisable] = useState(false)
+        const handleVisability = () =>{
+            setVisable(!visable)
+        }
+
     return (
         <Grid2 size={{ md: 6, xs: 12 }} bgcolor={theme.palette.grey[500]} paddingTop={4.5} marginBottom={4.5}>
             <Box textAlign={'center'}>
@@ -30,33 +34,59 @@ const SignUp = () => {
                 <Typography>Enjoy exclusive discounts & offers</Typography>
             </Box>
             <form action="" onSubmit={handleSignUp}>
-                <Box display={'flex'} alignItems={'end'}>
+                <Box display={'flex'} alignItems={'end'} marginBottom={'1rem'}>
                     <CircleIcon sx={{ mr: 1, my: 0.5 }} />
-                    <TextField sx={{ width: '100%' }} variant='standard' placeholder='First name' required value={name} onChange={(e) => setFirstName(e.target.value)} />
+                    <Input sx={{ width: '100%' }} variant='standard' placeholder='First name' required value={name} onChange={(e) => setFirstName(e.target.value)} />
                 </Box>
-                <Box display={'flex'} alignItems={'end'}>
+                <Box display={'flex'} alignItems={'end'} marginBottom={'1rem'}>
                     <CircleIcon sx={{ mr: 1, my: 0.5 }} />
-                    <TextField sx={{ width: '100%' }} variant='standard' placeholder='Last name' required value={lastname} onChange={(e) => setLastName(e.target.value)} />
+                    <Input sx={{ width: '100%' }} variant='standard' placeholder='Last name' required value={lastname} onChange={(e) => setLastName(e.target.value)} />
                 </Box>
 
-                <Box display={'flex'} alignItems={'end'}>
+                <Box display={'flex'} alignItems={'end'} marginBottom={'1rem'}>
                     <AlternateEmailIcon sx={{ mr: 1, my: 0.5 }} />
-                    <TextField sx={{ width: '100%' }} variant='standard' type='email' placeholder='E-mail Address' required value={email} onChange={(e) => setEmailUp(e.target.value)} />
+                    <Input sx={{ width: '100%' }} variant='standard' type='email' placeholder='E-mail Address' required value={email} onChange={(e) => setEmail(e.target.value)} />
                 </Box>
-                <Box display={'flex'} alignItems={'end'}>
+                <Box display='flex' alignItems='flex-end' marginBottom={'5rem'}>
                     <LockIcon sx={{ mr: 1, my: 0.5 }} />
-                    <TextField sx={{ width: '100%' }} variant='standard' type='password' placeholder='Last name' required value={password} onChange={(e) => setPasswordUp(e.target.value)} />
+                    <Input sx={{ width: '100%' }} placeholder='Password' variant='standard' type='password' value={password} onChange={(e) => setPassword(e.target.value)} required endAdornment={
+                        <InputAdornment>
+                            <IconButton onClick={handleVisability}>
+                                {!visable ? <VisibilityOffIcon /> : <Visibility /> }
+                            </IconButton>
+                        </InputAdornment>
+                    } />
                 </Box>
                 <Box>
                     <Typography fontSize={'18px'}>Yes, I would like to receive personalized offers, tips and tricks, and other information from Store.</Typography>
                     <Typography fontSize={'18px'}>The terms and conditions for Dubai Perfumes apply. Here you can find our privacy information.</Typography>
                 </Box>
                 <Button sx={{ textTransform: 'capitalize', width: '100%', borderRadius: '18px', color: "WHITE" }} variant='contained' color='warning' type='submit'>sign up</Button>
-                <Typography>Or</Typography>
-                <Typography>Sign up with google</Typography>
-                <FcGoogle />
+                <Box textAlign={'center'}>
+                    <Typography className="or"
+                        sx={{
+                            position: 'relative',
+                            '&::before, &::after': {
+                                content: '""',
+                                position: 'absolute',
+                                width: '40%',
+                                height: '1px',
+                                backgroundColor: theme.palette.common.black, // Or your desired color
+                                top: '10px',
+                            },
+                            '&::before': {
+                                left: 0,
+                            },
+                            '&::after': {
+                                right: 0,
+                            },
+                        }}
+                    >Or</Typography>
+                    <Typography sx={{ opacity: 0.5 }}>Sign up with google</Typography>
+                    <FcGoogle />
+                </Box>
             </form>
-        </Grid2>
+        </Grid2 >
     )
 }
 
